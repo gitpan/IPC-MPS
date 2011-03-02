2d901891ac388b6e08b5ffb7f14a8886ac7ae2ad
Nick Kostyria <kni@cpan.org>
2011-03-02 14:06:11 +0000

    Import of KNI/IPC-MPS-0.10 from CPAN.
    gitpan-cpan-distribution: IPC-MPS
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         KNI/IPC-MPS-0.10.tar.gz
    gitpan-cpan-author:       KNI
    gitpan-cpan-maturity:     released

--- IPC/MPS/Event.pm
+++ IPC/MPS/Event.pm
@@ -7,7 +7,7 @@ use Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(spawn receive msg snd wt snd_wt listener open_node);
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 use Carp;
 use Event;
